cd9e87071917dde483c7b963333c5d121a1cd351
Matthew Walker <matthew.r.walker4@gmail.com>
2021-03-28 21:37:42 -0500

    completed break and continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var userInputNum;
+
+while (true) {
+    userInputNum = parseInt(prompt("Enter an odd number between 1-50."));
+    if (userInputNum % 2 !== 0 && userInputNum < 50) {
+        console.log("Number to skip is: " + userInputNum);
+        break;
+    }
+}
+//Really gross looking loop with break and continue...
+for (let i = 0; i <= 100; ++i) {
+    if (i === 50) {
+        break;
+    } else if (i === userInputNum) {
+        console.log("Yikes! Skipping number: " + userInputNum);
+    } else if (i % 2 === 0) {
+        continue;
+    }
+    if (i === userInputNum) {
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
