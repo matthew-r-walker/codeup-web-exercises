712c4ab0e6badda1c3397913e3ca93ad452ec3ac
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-12 18:11:33 -0500

    saved progress on weather map exercise

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,27 +1,26 @@
+// getWeather retrieves weather data and displays it within html when called.
 var coords = [29.4241, -98.4936];
 function getWeather() {
     $.ajax('https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=' + coords[0] + '&lon=' + coords[1] + '&exclude=current,hourly,minutely&appid=' + WEATHER_MAP_TOKEN)
         .done(function (resp) {
 
-            // console.log(resp);
-
-            // Takes in the index for response daily object and returns the chance of rain for that index/day
+            // Takes in the index for resp.daily object and returns the chance of rain for that index/day.
             function chanceOfRain(index) {
                 var rainChanceDecimal = resp.daily[index].pop;
                 var rainChancePercentage = Math.round(rainChanceDecimal * 100) + '% Chance of precipitation';
                 return rainChancePercentage;
             }
-            // Takes in the index for response daily object and returns the humidity level for that index/day
+            // Takes in the index for resp.daily object and returns the humidity level for that index/day.
             function humidityLevel(index) {
                 var humidity = 'Humidity: ' + resp.daily[index].humidity + '%';
                 return humidity;
             }
-            // Takes in the index for response daily object and returns the weather description for that index/day
+            // Takes in the index for resp.daily object and returns the weather description for that index/day.
             function weatherDescription(index) {
                 var description = resp.daily[index].weather[0].description;
                 return description[0].toUpperCase() + description.substring(1);
             }
-            // Takes in the index for response daily object and returns the date for that index/day
+            // Takes in the index for resp.daily object and returns the date for that index/day.
             function dailyDate(index) {
                 var today = resp.daily[index];
                 var todayDate = new Date(today.dt * 1000).toString();
@@ -29,26 +28,26 @@ function getWeather() {
                 var date = [todayDate[0], todayDate[1], todayDate[2]].join(' ');
                 return date;
             }
-            // Takes in the index for response daily object and returns the weather icon for that index/day
+            // Takes in the index for resp.daily object and returns the weather icon for that index/day.
             function weatherIcon(index) {
                 var img = resp.daily[index].weather[0].icon;
                 var imgSrc = '<img src="http://openweathermap.org/img/w/'+ img +'.png">'
                 return imgSrc;
             }
-            // Takes in the index for response daily object and returns the high and low temperatures for that index/day
+            // Takes in the index for resp.daily object and returns the high and low temperatures for that index/day.
             function highAndLowTemp(index) {
                 var highTemp = Math.round(resp.daily[index].temp.max);
                 var lowTemp = Math.round(resp.daily[index].temp.min);
                 var highAndLowTemps = highTemp + `\xB0F / ` + lowTemp + '\xB0F';
                 return 'High/Low: ' + highAndLowTemps;
             }
-            /// This creates the on page html for weather display and adds the weather info to it
+            // This loop creates the on page html for weather display and adds the weather info to it.
             $('#weather-container').html('');
             for (var i = 0; i <= daysOfWeather; ++i) {
-                $('#weather-container').append('<div id="day-' + i + '" class="weather-daily flex-shrink-1"></div>')
+                $('#weather-container').append('<div id="day-' + i + '" class="weather-daily"></div>');
                 $('#day-' + i + '').html(
                     '<div class="card weather-card">' +
-                    '<div class="card-header font-weight-bolder">' + dailyDate(i) + '</div>' +
+                    '<div class="card-header font-weight-bold">' + dailyDate(i) + '</div>' +
                     '<ul class="list-group">' +
                     '<li class="list-group-item">' + weatherIcon(i) + '</li>' +
                     '<li class="list-group-item font-weight-bold">' + highAndLowTemp(i) + '</li>' +
@@ -60,18 +59,17 @@ function getWeather() {
             }
         });
 };
-getWeather();
 
-var daysOfWeather = 4;
 
+// Select drop down allows user to select how many days of weather forecast are displayed.
+var daysOfWeather;
 $('#select-options').change(function (){
     var selectVal = $('#select-options').val();
-    // console.log(selectVal);
     daysOfWeather = selectVal;
     getWeather();
 });
 
-///// map box stuff
+// Map box map
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 var map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -79,12 +77,13 @@ var map = new mapboxgl.Map({
     center: [-98.49272, 29.42527], // starting position [lng, lat]
     zoom: 8.7 // starting zoom
 });
+// Map zoom and orientation controller.
+map.addControl(new mapboxgl.NavigationControl());
 
-var latLngObj = {};
 
 var popup = new mapboxgl.Popup()
-    .setHTML("<h3>San Antonio, Texas, United States</h3>")
-    .setMaxWidth("300px");
+    .setHTML("<h5>San Antonio, Texas, United States</h5>")
+    .setMaxWidth("200px");
 
 var marker = new mapboxgl.Marker({
     color: '#1795d4'
@@ -94,6 +93,7 @@ var marker = new mapboxgl.Marker({
     .setPopup(popup)
     .addTo(map);
 
+// Updates weather and popup info on marker drag end.
 marker.on('dragend', function (){
     coords = marker.getLngLat().toArray().reverse();
     getWeather();
@@ -102,26 +102,23 @@ marker.on('dragend', function (){
 
 function updateMarkerPopUpInfo() {
     var lngLat = marker.getLngLat();
-    latLngObj = {lat: lngLat.lat, lng: lngLat.lng};
+    var latLngObj = {lat: lngLat.lat, lng: lngLat.lng};
     reverseGeocode(latLngObj, MAPBOX_ACCESS_TOKEN).then(function (results) {
         var arr = results.features;
         var place = arr.find(x => x.id.includes('place') === true);
-        marker._popup._content.innerHTML = '<h3>' + place.place_name + '</h3>';
-        // console.log(results);
+        marker._popup._content.innerHTML = '<h5>' + place.place_name + '</h5>';
     })
 }
 
 var textInput = '';
 $('button').click(function (){
     textInput = $('#text-input').val();
-    // console.log(textInput);
     moveMarkerToSearched();
 })
 
 function moveMarkerToSearched() {
     geocode(textInput, MAPBOX_ACCESS_TOKEN).then(function (results){
         marker.setLngLat(results);
-        // console.log(results);
         coords = marker.getLngLat().toArray().reverse();
         updateMarkerPopUpInfo();
         getWeather();
