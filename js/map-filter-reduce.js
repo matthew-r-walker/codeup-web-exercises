7593647de58e362d8445cf6432d45656f11a79ce
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-17 22:45:48 -0500

    completed map filter reduce exercise / saved progress on promises exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,27 +36,27 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
 let emails = users.map(user => user.email);
 // console.log(emails);
 
 let usersWithThreeOrMoreLangs = users.filter(user => user.languages.length >= 3);
+// console.log(usersWithThreeOrMoreLangs);
 
-// total's default value will be the first item in the array unless set to a value otherwise specified. It's first value is set to 0 in this example.
+// *Example* total's default value will be the first item in the array unless set to a value otherwise specified. It's first value is set to 0 in this example.
 // let totalOfAllUsersYearsOfExp = users.reduce((total, user) => {
 //     return total + user.yearsOfExperience;
 // },0);
 
-
 let totalOfAllUsersYearsOfExp = users.reduce((total, user) => total + user.yearsOfExperience, 0);
-// let totalOfAllUsersYearsOfExp = users.reduce((total, user) => {
-//    return total + user.yearsOfExperience;
-// },0);
+// console.log(totalOfAllUsersYearsOfExp);
 // Average years of experience for users is totalOfAllUsersYearsExp / users.length
 
-
+// Solution using an array that exists already
 // let longestUserEmail = emails.reduce((a, b) => a.length > b.length ? a : b);
 
-let longestUserEmail = users.reduce((a, b) => a.length > b.email.length ? a : b.email);
+let longestUserEmail = users.reduce((a, user) => a.length > user.email.length ? a : user.email);
+// console.log(longestUserEmail);
 
 let usersNames = users.reduce((accumulator, user, index, arr) => {
     index === arr.length -1 ? accumulator += `${user.name}.` : accumulator += `${user.name}, `;
@@ -64,4 +64,9 @@ let usersNames = users.reduce((accumulator, user, index, arr) => {
 },'Users names are: ');
 
 
+let uniqueListOfLangs = users.reduce((a, user) => {
+    user.languages.forEach(lang => !a.includes(lang) ? a.push(lang) : false);
+   return a;
+}, []);
+// console.log(uniqueListOfLangs);
 
--- js/promises.js
+++ js/promises.js
@@ -1,11 +1,27 @@
-
-
-
-
-function getGithubUsernames(userName) {
+// takes in an user name as string and console logs the last push date from that user.
+function getLastGitPush(userName) {
     return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GIT_HUB_TOKEN}})
         .then(response => response.json()).then(data => {
-            console.log(data);
-        })
+            let lastPushDate = new Date(data[0].created_at).toString();
+            console.log(lastPushDate);
+        }).catch(() => console.log('Fetch request failed, check if user name is correct.'));
 }
-getGithubUsernames('matthew-r-walker');
+// getLastGitPush('matthew-r-walker');
+
+
+function wait(num) {
+    console.log('This will connect in ' + num + ' seconds.');
+    return new Promise((fulfill, reject) => {
+        setTimeout(() => {
+        if (Math.random() > 0.5) {
+            fulfill('here is your data');
+        } else {
+            reject('network connection error');
+        }
+        }, num * 1000);
+    });
+}
+
+// wait(2)
+//     .then((message) => console.log('Success', message))
+//     .catch((message) => console.log('Failure to connect,', message));
