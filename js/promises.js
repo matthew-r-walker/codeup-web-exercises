4cb079d111e568cf86af51f9822e53d5c7c4e3c3
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-18 10:31:25 -0500

    refactored to check if event is pushevent

--- js/promises.js
+++ js/promises.js
@@ -2,13 +2,20 @@
 function getLastGitPush(userName) {
     return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GIT_HUB_TOKEN}})
         .then(response => response.json()).then(data => {
-            let lastPushDate = new Date(data[0].created_at).toString();
-            console.log(lastPushDate);
+            let lastPushDate;
+            for(let event of data) {
+                if(event.type === "PushEvent") {
+                    lastPushDate = new Date(event.created_at);
+                    break;
+                }
+            }
+            console.log(`${userName}'s last push was ${lastPushDate}`);
         }).catch(() => console.log('Fetch request failed, check if user name is correct.'));
 }
 // getLastGitPush('matthew-r-walker');
 
 
+// Accepts a number of seconds for timeout before returning promise
 function wait(num) {
     console.log('This will connect in ' + num + ' seconds.');
     return new Promise((fulfill, reject) => {
